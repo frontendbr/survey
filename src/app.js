73ded9618719bf44251685d485afc42d74b6a57b
LFeh <hi@felipefialho.com>
2018-09-25 19:50:07 -0300

    Init region

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import * as offline from 'offline-plugin/runtime';
 import './app.styl';
 import './components/welcome/welcome.js';
 import './components/introduction/introduction.js';
+import './components/region/region.js';
 import './components/footer/footer.js';
 
 
---
+++ src/components/region/region.js
@@ -0,0 +1,1 @@
+import './region.styl';
