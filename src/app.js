f0d321920e82d7b611ff5adc95198a96352acabd
LFeh <hi@felipefialho.com>
2018-09-26 12:20:40 -0300

    Add chartjs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import * as offline from 'offline-plugin/runtime';
+import Chart from 'chart.js';
+
 import './app.styl';
 import './components/welcome/welcome.js';
 import './components/introduction/introduction.js';
 import './components/region/region.js';
 import './components/footer/footer.js';
-
 
 offline.install({
   onUpdateReady: function() {
