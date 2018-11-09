7b50b75eda677162ea1c59d93a971270687ff9b9
LFeh <hi@felipefialho.com>
2018-11-09 01:27:18 -0200

    Add function to generate

--- src/components/region/region.js
+++ src/components/region/region.js
@@ -1,38 +1,7 @@
-import './region.styl'; 
-import Chart from 'chart.js';
-import Api from './region.json';
-import RandomColor from 'randomColor';
-import { ConvertToPercentage } from '../../js/convert-to-percentage';
+import './region.styl';  
+import Api from './region.json'; 
+import { ChartDoughnut } from '../../js/chart-doughnut'; 
 
 const $selector = document.querySelector('[data-chart="region"]'); 
  
-const getLabels = () => Api.map((item, i) => `${item.answer}: ${ConvertToPercentage(getValues()[i])}`);
-
-const getValues = () => Api.map(item => item.count);
-
-const arrayCount = getValues().length;
-
-const randomBlue = () => RandomColor({ 
-  count: arrayCount,
-  hue: '#5891ff'
-});
-
-const startChart = ()=> new Chart($selector, {
-  type: 'doughnut',
-  data: {
-    labels: getLabels(),
-    datasets: [{ 
-      data: getValues(),
-      backgroundColor: randomBlue(),
-      borderColor: '#fff',
-      borderWidth: 2
-    }]
-  }, 
-  options: {
-    legend: {
-      position: 'left'
-    }
-  }
-});
-
-startChart();
+ChartDoughnut({ api: Api, selector: $selector });
