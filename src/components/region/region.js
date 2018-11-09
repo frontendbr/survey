0a23e258f340839da53e43ebc6ab67b3c070c0ab
LFeh <hi@felipefialho.com>
2018-11-09 00:18:11 -0200

    Create regions

--- src/components/region/region.js
+++ src/components/region/region.js
@@ -1,1 +1,38 @@
-import './region.styl';
+import './region.styl'; 
+import Chart from 'chart.js';
+import Api from './region.json';
+import RandomColor from 'randomColor';
+import { ConvertToPercentage } from '../../js/convert-to-percentage';
+
+const $selector = document.querySelector('[data-chart="region"]'); 
+ 
+const getLabels = () => Api.map((item, i) => `${item.answer}: ${ConvertToPercentage(getValues()[i])}`);
+
+const getValues = () => Api.map(item => item.count);
+
+const arrayCount = getValues().length;
+
+const randomBlue = () => RandomColor({ 
+  count: arrayCount,
+  hue: '#5891ff'
+});
+
+const startChart = ()=> new Chart($selector, {
+  type: 'doughnut',
+  data: {
+    labels: getLabels(),
+    datasets: [{ 
+      data: getValues(),
+      backgroundColor: randomBlue(),
+      borderColor: '#fff',
+      borderWidth: 2
+    }]
+  }, 
+  options: {
+    legend: {
+      position: 'left'
+    }
+  }
+});
+
+startChart();
