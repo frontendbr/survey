3e4a24be863d972582201ddef1dfbed79d41b0a8
LFeh <hi@felipefialho.com>
2018-11-09 01:35:33 -0200

    Fix text

---
+++ src/components/job/genre.js
@@ -0,0 +1,7 @@
+import './job.styl';  
+import Api from './job.json'; 
+import { ChartDoughnut } from '../../js/chart-doughnut'; 
+
+const $selector = document.querySelector('[data-chart="job"]'); 
+ 
+ChartDoughnut({ api: Api, selector: $selector });
